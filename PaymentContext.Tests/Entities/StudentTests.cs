cecf0476dc8d823c0411625c0b8c5608970265bf
renanbernardo <renan.brnrd@gmail.com>
2021-06-04 14:08:10 -0300

    Corrupção no Código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Renan", "Bernardo", "1234567890", "email@teste.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
